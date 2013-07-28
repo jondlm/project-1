3aa444a843025e6376ad9010ae33d6244c325554
Andrea de la Motte <andreahill@jon.local>
2013-07-28 11:20:30 -0700

    templates probably working

--- index.js
+++ index.js
@@ -1,43 +1,20 @@
-
-
-
-
 function template(templateString){
-  var matches = templateString.match(/\{\{\s*(\w+)\s*\}\}/g),
-      rendered = '';
+  var matches = templateString.match(/\{\{\s*\w+\s*\}\}/g)
+    , rendered = templateString;
 
   function transform(obj){
-    if (!obj) {return null};
-
-    var keys = Object.keys(obj);
-
-    // For each of the input keys, loop through and swap them
-    keys.forEach(function(key){
-      matches.filter(function(i){
-        i.match(/\w+/) === key ? true : false;
-      });
-
-      rendered = rendered.replace(item);
-
+    matches.forEach(function(item){
+      var key = item.match(/\w+/);
+      if (key && obj[key])
+        rendered = rendered.replace(item, obj[key]);
     });
-
     return rendered;
   }
   return transform;
 }
-
 
 /****************************************
                  EXPORTS
 ****************************************/
 
 module.exports = template;
-
-
-
-// var tpl = your_function("hello {{ world }} how is your {{ weekday }} going?")
-
-// tpl({
-//   world: 'dude'
-// , weekday: 'tuesday'
-// })  // == "hello dude how is your tuesday going?"
